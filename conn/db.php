51073959c9b99a1f715837f45e33fc0efde7afc2
Sabnock <kingsabnock@gmail.com>
2025-02-17 22:49:36 -0800

    initial commit

---
+++ conn/db.php
@@ -0,0 +1,16 @@
+<?php
+$host = "localhost";  // Server (localhost in XAMPP)
+$user = "root";       // Default XAMPP username
+$pass = "";           // Default XAMPP password (empty)
+$dbname = "Communitap"; // Change to your database name
+
+// Create connection
+$conn = new mysqli($host, $user, $pass, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} else {
+    echo "Connected successfully";
+}
+?>
---
+++ landingpage.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<div class="landingpage">
+  <div class="communi-tap">CommuniTAP</div>
+  <div class="helping-the-neighborhood-with-fun-tasks">
+    Helping the neighborhood with fun tasks
+  </div>
+  <div class="frame-427320603">
+    <div class="frame-427320600">
+      <div class="group-427320600">
+        <div class="rectangle-1190"></div>
+        <img class="bi-check" src="bi-check0.svg" />
+      </div>
+      <div class="help-to-level-up">Help to level UP</div>
+    </div>
+    <div class="frame-427320601">
+      <div class="group-427320600">
+        <div class="rectangle-1190"></div>
+        <img class="bi-check2" src="bi-check1.svg" />
+      </div>
+      <div class="tap-and-pass-the-completed-task">
+        Tap and pass the completed task
+      </div>
+    </div>
+    <div class="frame-427320602">
+      <div class="group-427320600">
+        <div class="rectangle-1190"></div>
+        <img class="bi-check3" src="bi-check2.svg" />
+      </div>
+      <div class="earn-rewads">Earn rewads!</div>
+    </div>
+  </div>
+  <img class="human-logo-1" src="human-logo-10.png" />
+  <div class="button">
+    <img class="thumbs-up" src="thumbs-up0.svg" />
+    <div class="take-a-task-now">Take a task now!</div>
+  </div>
+</div>
+</body>
+</html>
